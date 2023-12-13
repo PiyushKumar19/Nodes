9a74138d5b0afd60ea74dfa31f9c4134e13edfa0
Piyush <developerpiyush1102k2@gmail.com>
2023-12-13 22:41:02 +0530

    Learned about getting data from headers, params, and body. Sending string, json, and html responses.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
+// To get data from the response body
+var bodyParser = require("body-parser");
 const port = 3000;
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+
+// First middleware is called the request route handler
+function middleware1(req, res, next) {
+   // Two reposnses cannot be sent, like one from middleware without any condition and
+   // another from the request route handler.
+   
+   console.log("from inside of Middleware 1"+req.headers.counter);
+   
+   // next -> this method is important, only after this is called,
+   // next method or the route handler will be called.
+   next();
+}
+// app.use -> Only when this is used then middleware gets added to the request pipeline
+app.use(middleware1);
 
  function calculateSum (counter) {
     var sum = 0;
@@ -11,13 +28,15 @@ const port = 3000;
     return sum;
  }
 
- function handleFirstRequest(req, resp) {
+ function handleParams(req, resp) {
     // Taking counter data from params
     let counter = req.params.counter;
 
-    let sum = calculateSum(counter);
-    let str = 'The sum is '+sum;
-    resp.send(str);
+    let answerObject = {
+      sum: calculateSum(counter)
+    }
+    // returning object as response
+    resp.send(answerObject);
  }
 
  function startedMessage() {
@@ -28,14 +47,54 @@ const port = 3000;
    // req.headers.counter -> collects the counter value from the headers in post request
    let counter = req.headers.counter;
    console.log(req.headers);
-   let sum = calculateSum(counter);
-   let str = 'The sum is '+sum;
 
-   res.send(str);
+   if (counter < 100000) {
+      let sum = calculateSum(counter);
+      let str = 'The sum is '+sum;
+      res.send(str);
+   } else {
+      // sending a custom status code with function currying ("calling of multiple functions at one time in a order like,")
+      // res.status(411).send("...");
+      res.status(411).send("You have sent a very big number");
+   }
+
  }
 
- app.get('/:counter', handleFirstRequest);
+ function calculateMul(counter) {
+   let answer = 1;
+   for (let i = 1; i<=counter; i++) {
+      answer = answer * i;
+   }
+   return answer
+ }
 
- app.post('/handleHeaders', handleHeaders);
+ function handleBody( req, res) {
+   console.log(req.body);
+   let counter = req.body.counter;
+   let result = {
+      sum: calculateSum(counter),
+      mul: calculateMul(counter),
+   };
+   res.send(result);
+ }
+
+ function handleHtml(req, res) {
+   /*
+   res.send(`
+      <body>
+         <b>hi there</b>
+      </body>`);
+   */
+   res.sendFile(__dirname+"./index.html");
+ }
+ app.get('/getHtml', handleHtml);
+
+ 
+ app.get('/:counter', handleParams);
+
+ // middlewares can be called for a particular route
+ app.post('/handleHeaders', middleware1, handleHeaders);
+ 
+ app.post('/handleBody', handleBody);
  
  app.listen(port, startedMessage);
